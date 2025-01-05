78edab99d9783f4e7fdec1e55dc42dfcc06d1f3a
piksl1 <karimmohsin10@gmail.com>
2025-01-05 17:46:36 +0100

    portable

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import Sidebar from "./components/sidebar";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-700 text-center">
-        Hello world!
-      </h1>
+      <Sidebar />
     </>
   );
 }
